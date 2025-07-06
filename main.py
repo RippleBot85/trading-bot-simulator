24e46e524649cb6439fc5695932b03529afae2c5
RippleBot85 <bitstampbotripple@gmail.com>
2025-07-06 14:35:49 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,1 @@
-from fastapi import FastAPI
-import simulate  # Assicura che questo abbia la funzione run_bot()
 
-app = FastAPI()
-
-@app.get("/")
-def root():
-    return {"message": "Bot attivo!"}
-
-@app.get("/ping")
-def ping():
-    simulate.run_bot()
-    return {"status": "ok", "detail": "Simulazione eseguita"}
